446baa3efe2252b3d7011b30770471802293e792
Taylor-Stacey <taylor88stacey@gmail.com>
2022-06-28 14:21:16 -0700

    done

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react';
+import Spinner from './Spinner'
+import {render, screen, waitFor} from '@testing-library/react';
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
+
+test('test that spinner renders without errors', () => {
+  render(<Spinner />);
+});
